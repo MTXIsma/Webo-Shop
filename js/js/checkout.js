07863d6c3895f6093fa5b4b02154e102165b98ef
IswoxX <isma.boite.pro@gmail.com>
2026-02-11 22:07:03 +0100

    Delete js directory

--- js/js/checkout.js
+++
@@ -1,4 +0,0 @@
-// Si tu veux plus tard intégrer le vrai PayPal sandbox ou crypto
-// Ici juste des alertes simulant le paiement
-export function checkoutPaypal(total){ alert("PayPal: " + total + "€"); }
-export function checkoutCrypto(total){ alert("Crypto: " + total + "€"); }
